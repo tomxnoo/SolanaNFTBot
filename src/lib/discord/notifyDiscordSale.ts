b31cac12fbf5ab9eb67e332aaab39268700dd5d1
tomxnoo <96437777+tomxnoo@users.noreply.github.com>
2021-12-21 15:38:51 +0200

    notifysale

--- src/lib/discord/notifyDiscordSale.ts
+++ src/lib/discord/notifyDiscordSale.ts
@@ -21,18 +21,42 @@ export default async function notifyDiscordSale(
     return;
   }
 
-  const { marketplace, nftData } = nftSale;
+    const { marketplace, nftData } = nftSale;
   const description = `Sold for ${nftSale.getPriceInSOL()} S◎L at ${
     marketplace.name
   }`;
+
   const embedMsg = new MessageEmbed({
     color: "#0099ff",
-    title: nftData?.name,
-    description,
-    url: marketplace.itemURL(nftSale.token),
+    username: "Pepe Sales Bot",
     thumbnail: {
       url: nftData?.image,
     },
+    const embedMsg = new MessageEmbed({ [
+      {
+        author: {
+          title: nftData?.name,
+        description,
+          url: marketplace.itemURL(nftSale.token)",
+          icon_url: https://i.imgur.com/R66g1Pe.jpg"
+        },
+        url: marketplace.itemURL(nftSale.token)",
+        "description":blank,
+        "fields": [
+          {
+            "name": "Buyer",
+            "value": "Wallet",
+            "inline": true
+          },
+          {
+            "name": "Seller",
+            "value": "Wallet",
+            "inline": true
+          },
+          {
+            "name": "Date here",
+            "value": "okay..."
+          },
   });
   await channel.send({
     embeds: [embedMsg],
